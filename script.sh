78a6940850b960b29219b857019dd7d9ca1057e7
Júnior Marcondes <marc@mm.local>
2022-11-19 21:38:00 -0300

    codding the script

--- script.sh
+++ script.sh
@@ -1,6 +1,25 @@
 #!/bin/zsh
 
-zparseopts -E -D -- \
-           t:=o_time\
-	   q:=o_quant\
+  local flag_help flag_list flag_recents
+  local arg_filename=$(pwd)
+  local arg_time=1440 arg_choose=1 arg_quant=1
+  local usage=(
+    "[-h|--help]"
+    "[-v|--verbose] [-f|--filename=<file>] [<message...>]"
+  )
 
+zmodload zsh/zutil
+zparseopts -D -F -K -- \
+  {h,-help}:=flag_help \
+  {s,-show-recents}:=flag_recents \
+  {l,-list}l:=flag_list \
+  {t,-time}:=arg_time \
+  {c,-choose}:=arg_choose \
+  {q,-quantity}:=arg_quant \
+  {f,-filename}=arg_filename || return 1
+
+echo "--help: $flag_help"
+echo "--logs: $flag_logs"
+echo "--list: $flag_list"
+echo "--filename: $arg_filename"
+echo "positional: $@"
